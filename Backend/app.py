aa9c1875d2c8a8b7e895a8b711ac17e3f30bc352
ZethGecko <nelsonluque098@gmail.com>
2025-06-02 17:57:38 -0500

    implementacion vista-catalogo

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import cliente
 import almacen
 import catalogo
@@ -22,6 +23,7 @@ import ventas
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def inicio():
