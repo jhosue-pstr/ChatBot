fc3440587e6aa0afae9320b082c4d38865355e8e
Moises-Mestas <moisesjoaquin51@gmail.com>
2025-06-03 00:14:05 -0500

    Descripción de los cambios

--- Frontend/app.py
+++ Frontend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('catalogo.html')  
+    return render_template('principal.html')  
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
